ad718ca455b8d664ed3ebc3be36e940618d6422d
pedro <pedrosbmartinazzo@hotmail.com>
2024-08-12 08:17:41 -0300

    commit inicial

---
+++ src/test/java/com/psbm/sphere/SphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psbm.sphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
